ff3adc5856fc96b6f01b88153061a98450a6e8f6
quintrix <167034113+quintriix@users.noreply.github.com>
2024-12-17 20:18:16 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,2 @@
-game:GetService("StarterGui"):SetCore("SendNotification", {
-	Title = "Nation.Dahood | quintrix",
-	Text = "Soon...", 
-	Duration = 3
-})
+local NationNotify = loadstring(game:HttpGet("https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/notify.lua"))()
+NationNotify:Notify("Nation.loader", "Soon...", 3)
