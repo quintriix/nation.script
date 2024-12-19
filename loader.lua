f4a5ee5c3cdcaacbe777b018abdaa9796bbad3df
quintrix <167034113+quintriix@users.noreply.github.com>
2024-12-19 17:01:03 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,8 +2,7 @@ local games = {
     [{2788229376}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- DaHood
     [{4483381587}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- BasePlate
     [{103539521748777}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- FlameHood
-    [{88582222971530}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- DelHood
-    [{286090429}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood" -- Arsenal
+    [{88582222971530}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood" -- DelHood
 }
 
 for ids, url in next, games do
