ddb72e0b0869e55a59f87c3b8a95bdf7272ba59a
quintrix <167034113+quintriix@users.noreply.github.com>
2024-12-22 13:37:26 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,13 +1,1 @@
-local games = {
-    [{2788229376}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- DaHood
-    [{4483381587}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- BasePlate
-    [{103539521748777}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- FlameHood
-    [{88582222971530}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood" -- DelHood
-}
-
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))()
-        break
-    end
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood"))()
