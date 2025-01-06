6be54422f8e0f937e9bb69a987d48e33a171bc75
quintrix <167034113+quintriix@users.noreply.github.com>
2025-01-06 15:52:48 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,19 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood"))()
+
+
+pcall(function()
+    local inviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/extras/source.lua"))()
+    inviter.Join("discord.gg/qm2KAzZxA9")
+end)
+
+local games = {
+    [{2788229376}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/nation.dahood", -- DA HOOD
+    [{3956818381}] = "", -- NINJA LEGENDS
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
