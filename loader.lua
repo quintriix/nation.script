82ba501b81479745783104c22bdce0fb6d2c3c2c
quintrix <167034113+quintriix@users.noreply.github.com>
2025-01-07 17:48:05 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,6 +6,7 @@ end)
 local games = {
     [{2788229376}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/scripts/dahood.lua", -- DA HOOD
     [{3956818381}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/scripts/ninjalegends.lua", -- NINJA LEGENDS
+    [{1345139196}] = "https://raw.githubusercontent.com/quintriix/nation.script/refs/heads/main/addoins/scripts/Nation.TreasureHuntSimulatorBeta.lua", -- TreasureHuntSimulator [BETA
 }
 
 for ids, url in next, games do
